904fe19236ec62f8278f48f8084181eff922619f
rschwieger <robert.schwieger@gmail.com>
2026-03-06 14:09:56 +0100

    bewegung fix

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -20,6 +20,7 @@ public class GamePanel extends JPanel {
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+        setFocusable(true);
 
     }
     public void changeXDelta (int value){
