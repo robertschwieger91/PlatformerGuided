511202f6d1eb15769613b7b90aa0233e0d87d46c
robertschwieger91 <robert.schwieger@gmail.com>
2026-03-12 21:26:53 +0100

    Ausgabeframes und Updateframes getrennt

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,6 +6,8 @@ public class Game implements Runnable {
 	private GamePanel gamePanel;
 	private Thread gameThread;
 	private final int FPS_SET = 120;
+	private final int UPS_SET = 200;
+
 
 	public Game() {
 
@@ -21,29 +23,52 @@ public class Game implements Runnable {
 		gameThread.start();
 	}
 
+	public void update(){
+		gamePanel.updateGame();
+	}
+
 	@Override
 	public void run() {
 
 		double timePerFrame = 1000000000.0 / FPS_SET;
+		double timePerUpdate = 1000000000.0 / UPS_SET;
 		long lastFrame = System.nanoTime();
 		long now = System.nanoTime();
 
+		long previousTime = System.nanoTime();
+
 		int frames = 0;
+		int updates = 0;
 		long lastCheck = System.currentTimeMillis();
 
-		while (true) {
+		double deltaU = 0;
+		double deltaF = 0;
 
+		while (true) {
 			now = System.nanoTime();
-			if (now - lastFrame >= timePerFrame) {
+			long currentTime = System.nanoTime();
+
+			deltaU += (currentTime - previousTime) / timePerUpdate;
+			deltaF += (currentTime - previousTime) / timePerFrame;
+			previousTime = currentTime;
+
+			if (deltaU >= 1) {
+				update();
+				updates++;
+				deltaU--;
+			}
+
+			if (deltaF >= 1) {
 				gamePanel.repaint();
-				lastFrame = now;
 				frames++;
+				deltaF--;
 			}
 
 			if (System.currentTimeMillis() - lastCheck >= 1000) {
 				lastCheck = System.currentTimeMillis();
-				System.out.println("FPS: " + frames);
+				System.out.println("FPS: " + frames + " || " + updates);
 				frames = 0;
+				updates = 0;
 			}
 		}
 
